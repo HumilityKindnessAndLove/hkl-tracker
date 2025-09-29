3be8f2bde515538ffdb5673513143a666b972bf2
manrajsinghvirk <156729352+manrajsinghvirk@users.noreply.github.com>
2025-09-29 11:10:25 -0600

    feat: add hkl pledge form (#9)
    * updated the code
    
    * created the basic overlay of the HKL form
    
    * implmented hkl brevo form
    
    * fixed the width of the dropdown menus
    
    * refactor: hkl form to follow existing formatting
    
    ---------
    
    Co-authored-by: squink-dev <squireink13@gmail.com>

--- app/(app)/form/page.tsx
+++ app/(app)/form/page.tsx
@@ -1,23 +1,9 @@
-import { Card, Heading, Text } from "@radix-ui/themes";
+import HKLForm from "@/components/HKLForm";
 
 export default function FormPage() {
   return (
-    <div className="space-y-6">
-      <Heading size="6" className="text-center">
-        HKL Form
-      </Heading>
-
-      <Card size="3">
-        <div className="space-y-4">
-          <Text as="div">
-            This is the HKL Form page where users/volunteers can fill out forms
-            directly.
-          </Text>
-          <Text as="div" color="gray">
-            Form components and functionality will be implemented here.
-          </Text>
-        </div>
-      </Card>
+    <div className="p-6">
+      <HKLForm />
     </div>
   );
 }
