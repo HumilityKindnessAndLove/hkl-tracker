618bd86e9716a292ce806f547c2058f4537f910c
squink-dev <squireink13@gmail.com>
2025-10-08 23:21:04 -0600

    remove extra layout padding

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,7 +1,3 @@
 export default function AppLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <main>
-      <div className="mx-auto max-w-4xl">{children}</div>
-    </main>
-  );
+  return <main>{children}</main>;
 }
