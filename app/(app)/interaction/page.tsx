ae1c3ec0f4c837fe2b3bf3215e2854e3b5b04118
squink-dev <squireink13@gmail.com>
2025-10-09 22:26:05 -0600

    fix padding

--- app/(app)/interaction/page.tsx
+++ app/(app)/interaction/page.tsx
@@ -2,7 +2,7 @@ import InteractionForm from "@/components/InteractionForm";
 
 export default function InteractionPage() {
   return (
-    <div className="space-y-6 px-2">
+    <div className="p-6 md:pt-24 pb-24 md:pb-6">
       <InteractionForm />
     </div>
   );
