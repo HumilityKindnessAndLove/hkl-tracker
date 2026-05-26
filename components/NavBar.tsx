31cf8ce8c2dc0fb868e11b5947db187e4be3bfde
manrajsinghvirk <156729352+manrajsinghvirk@users.noreply.github.com>
2026-05-26 01:37:08 -0600

    Update NavBar.tsx
    Change the form icon from a paper airplane to a pencil

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,19 +1,19 @@
 "use client";
 
-import { Home, Send, User } from "lucide-react";
+import { Pencil, User, Home } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
 const items = [
-  { href: "/form", label: "HKL Form", icon: Send },
+  { href: "/form", label: "HKL Form", icon: Pencil   },
   // { href: "/interaction", label: "Interaction", icon: Pencil },
   { href: "/profile", label: "Profile", icon: User },
 ];
 
 const mobileItems = [
-  { href: "/form", label: "Form", icon: Send },
+  { href: "/form", label: "Form", icon: Pencil   },
   { href: "/profile", label: "Profile", icon: User },
 ];
 
